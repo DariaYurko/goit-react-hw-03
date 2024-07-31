f014ea4f9201142a0840e6d91147ab2d6f3d272d
DariaYurko <dariayurkodev@gmail.com>
2024-07-31 16:08:36 +0300

    feat: add contacts.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
+import initialContacts from './contacts.json';
 import { useState } from 'react';
 import './App.css';
 import ContactList from './components/ContactList/ContactList';
 
-const contacts = [
+const [contacts, setContacts] = useState([
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
-// const [contacts, setContact] = useState([
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ]);
+]);
 
 const App = () => {
   return (
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,18 @@
-import Contact from "../Contact/Contact";
-import css from './ContactsList.module.css'
-const ContsctList = ({ contacts }) => {
-   return (
-      <ul className={css.contactList}>
-         {contacts.map(contact => {
-            return (
-              <li key={contact.id} className={css.contact}>
-                <Contact name={contact.name} phone={contact.number} />
-              </li>
-            );
-         })}
-      </ul>
-   )
-}
+import Contact from '../Contact/Contact';
+import css from './ContactsList.module.css';
 
-export default ContsctList
+const ContsctList = ({ contacts }) => {
+  return (
+    <ul className={css.contactList}>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id} className={css.contact}>
+            <Contact name={contact.name} phone={contact.number} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContsctList;
