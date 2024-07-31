60490dc1e0723d1f2028ace5e73b8e843cf49625
DariaYurko <dariayurkodev@gmail.com>
2024-07-31 16:12:50 +0300

    feat: update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
+import initialContacts from './contacts.json'
 import { useState } from 'react';
 import './App.css';
 import ContactList from './components/ContactList/ContactList';
 
-const contacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
-// const [contacts, setContact] = useState([
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ]);
+// const [contacts, setContacts] = useState(initialContacts);
 
 const App = () => {
   return (
@@ -24,7 +13,7 @@ const App = () => {
       <SearchBox />
       <ContactList /> */}
 
-      <ContactList contacts={contacts} />
+      <ContactList contacts={initialContacts} />
     </div>
   );
 };
